9d156342c2d1b22467c943908ebb8b3646091f29
pavan <pashampavan02@gmail.com>
2024-12-31 10:06:35 +0530

    React refresh problems solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
 import SignIn from './Components/SignIn';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom'; // Use HashRouter
 import Dashboard from './Components/Dashboard';
 import TaskList from './Components/TaskList';
 import Context from './Context/Context';
+
 function App() {
   return (
     <Context>
-    <div className="App">
-      <Navbar/>
-      <Routes >
-        <Route  path='/*' element={<SignIn type="signin"/>}/>
-        <Route  path='/' element={<SignIn type="signin"/>}/>
-        <Route  path='/task-management-frontend' element={<SignIn type="signin"/>}/>
-        <Route  path='/dashboard' element={<Dashboard/>}/>
-        <Route  path='/tasklist' element={<TaskList/>}/>
-        <Route  path='/signup' element={<SignIn type="signup"/>}/>
-      </Routes >
-    </div>
-
+      <div className="App">
+        <>
+          <Navbar />
+          <Routes>
+            {/* Default route */}
+            <Route path="/" element={<SignIn type="signin" />} />
+            {/* Routes for other paths */}
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/tasklist" element={<TaskList />} />
+            <Route path="/signup" element={<SignIn type="signup" />} />
+            {/* Wildcard route */}
+            <Route path="*" element={<SignIn type="signin" />} />
+          </Routes>
+        </>
+      </div>
     </Context>
   );
 }
